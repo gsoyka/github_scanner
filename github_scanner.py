db366fd975205fa7ef8e8473cfbca8b71bd65489
Grant Soyka <gsoyka@gmail.com>
2019-09-11 07:39:44 -0600

    Fix report not getting generated

--- github_scanner.py
+++ github_scanner.py
@@ -48,29 +48,30 @@ def get_repo_names(github, scan_private, org=None):
 def write_to_file(output, repo_name):
     global datestamp
     # Define the directory to write to as a folder named templates in the current dir
-    dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'reports'))
+    output_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'reports'))
 
     # Create the directory if it does not exist
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-        # Define filename
-        filename = re.sub('\.py$','', sys.argv[0])
-        file = os.path.join(dir,filename)
+    # Define filename
+    filename = re.sub('\.py$','', sys.argv[0])
+    output_file = os.path.join(output_dir,filename)
 
-        # Format our output
+    # Format our output
+    try:
         output = output.decode('utf-8')
         output = re.sub('\[92m', '', output)
         output = re.sub('\[93m', '', output)
         output = re.sub('\[0m', '', output)
+    except (UnicodeDecodeError, AttributeError):
+        pass
 
-        # Write the stdout to txt file
-        target = open(file + "-" + datestamp + '.txt', 'a')
-        target.truncate()
+    # Write the stdout to txt file
+    with open(f"{output_file}-{datestamp}.txt", 'a') as target:
         target.write(repo_name + "\n")
         target.write(output)
         target.write("\n")
-        target.close()
 
 
 def scan_repos(repos):
@@ -89,27 +90,24 @@ def scan_repos(repos):
                 stderr=subprocess.STDOUT,
                 universal_newlines=True
             )
-            # If the output is not empty, trufflehog found a problem, so
-            # add the repo name to our list of problem repos, and write the
-            # output to our report file
-            if len(output) != 0:
-                print("\033[1;31;40m Problems found in: " + repo_name)
-                problem_repos.append(repo_name)
-                write_to_file(output, repo_name)
-
+        # Trufflehog will exit non-zero if it found a problem, so
+        # add the repo name to our list of problem repos, and write the
+        # output to our report file
         except subprocess.CalledProcessError as e:
-            print("\033[1;31;40m Problems found in repo: " + repo_name)
+            print("\033[1;31;40m Problems found in: " + repo_name)
             print(e.output)
+            problem_repos.append(repo_name)
+            write_to_file(e.output, repo_name)
         except Exception as e:
             print("\033[1;31;40m Problem checking repo: " + repo_name)
 
-        # Report on our findings
-        if len(problem_repos) != 0:
-            print("\n")
-            print("\033[1;31;40m Problems detected in " + str(len(problem_repos)) + " repository(s) which are listed below")
-            print("\033[1;31;40m Please review the generated report for additional detail")
-            for repo in problem_repos:
-                print("\033[1;31;40m" + repo)
+    # Report on our findings
+    if len(problem_repos) != 0:
+        print("\n")
+        print("\033[1;31;40m Problems detected in " + str(len(problem_repos)) + " repository(s) which are listed below")
+        print("\033[1;31;40m Please review the generated report for additional detail")
+        for repo in problem_repos:
+            print("\033[1;31;40m " + repo)
 
 
 ######################## MAIN BEGINS HERE ###############################
